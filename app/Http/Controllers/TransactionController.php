ebc215e50647d56e01b5af02bd18742d51c6ad4c
Rodrick Malama <rodrick.kmal@outlook.com>
2024-04-06 09:49:44 +0200

    Second push

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+   public function create(Request $request)
+   {
+       return 'Hello World';
+   }
+
+   public function delete(Request $request)
+   {
+       # code...
+   }
+
+   public function update(Request $request)
+   {
+       # code...
+   }
+
+   public function transaction($id)
+   {
+       # code...
+   }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TransactionController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/api/deposit', [TransactionController::class, 'create']);
